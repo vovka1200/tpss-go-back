9bb89fef1b422f8539b3d2ef650fae28a791ad31
Vladimir Yavorskiy <vovka@krevedko.su>
2025-07-16 19:27:32 +0300

    users

--- api/v1/access/access.go
+++ api/v1/access/access.go
@@ -2,13 +2,16 @@ package access
 
 import (
 	"github.com/vovka1200/tpss-go-back/api/v1/access/login"
+	"github.com/vovka1200/tpss-go-back/api/v1/access/users"
 	"github.com/vovka1200/tpss-go-back/jsonrpc2"
 )
 
 type Access struct {
-	Login login.Login
+	Login login.Login `json:"login"`
+	Users users.Users `json:"users"`
 }
 
 func (a *Access) Register(methods jsonrpc2.Methods) {
-	methods["login"] = a.Login.Handler
+	a.Login.Register(methods)
+	a.Users.Register(methods)
 }
